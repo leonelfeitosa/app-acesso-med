3b67e81c4faa0bd7fa407a2d80fd7d0cb6f551a1
Samuel Martins <samuel.ubuntu@gmail.com>
2019-08-16 16:14:14 -0300

    Apresentar botao de responsavel baseado na idade do cliente

--- src/app/cadastrar-cliente/cadastrar-cliente.page.ts
+++ src/app/cadastrar-cliente/cadastrar-cliente.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { NavController } from '@ionic/angular';
 
+
+
 @Component({
   selector: 'app-cadastrar-cliente',
   templateUrl: './cadastrar-cliente.page.html',
@@ -17,12 +19,18 @@ export class CadastrarClientePage implements OnInit {
     data_nascimento: new FormControl('', Validators.required)
   });
   responsavel: any = {};
+  precisaResponsavel = false;
 
   constructor(private navCtrl: NavController) { }
 
   ngOnInit() {
+    localStorage.removeItem('responsavel');
+  }
+
+  ionViewWillEnter() {
     this.responsavel = JSON.parse(localStorage.getItem('responsavel'));
-    
+    localStorage.removeItem('responsavel');
+    console.log(this.responsavel);
   }
 
   cadastrarResp(){
@@ -33,5 +41,26 @@ export class CadastrarClientePage implements OnInit {
     console.log(this.clienteGroup.value.data_nascimento);
     console.log(this.responsavel);
   }
+
+  calcularIdade() {
+    const idade = this.getAge(this.clienteGroup.value.data_nascimento);
+    if (idade < 18) {
+      this.precisaResponsavel = true;
+    } else {
+      this.precisaResponsavel = false;
+    }
+  }
+
+  getAge(dateString) {
+    const today = new Date();
+    const birthDate = new Date(dateString);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) 
+    {
+        age--;
+    }
+    return age;
+}
 
 }
