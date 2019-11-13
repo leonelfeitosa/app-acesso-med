3024a1c217a5d8f1202ddf6d28c4e0ca9ab8455f
Samuel Martins <samuel.ubuntu@gmail.com>
2019-11-13 18:14:55 -0300

    Ultimas tarefas

--- src/app/interceptors/apiinterceptor.interceptor.ts
+++ src/app/interceptors/apiinterceptor.interceptor.ts
@@ -8,7 +8,7 @@ export class APIInterceptor implements HttpInterceptor {
     private apiUrlProd = 'http://162.243.161.30:3021';
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         if (!req.url.startsWith('https://')) {
-            const apiReq = req.clone({ url: `${this.apiUrlTest}/${req.url}` });
+            const apiReq = req.clone({ url: `${this.apiUrlProd}/${req.url}` });
             return next.handle(apiReq);
         } else {
             return next.handle(req);
